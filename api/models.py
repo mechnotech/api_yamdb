1c6cdbd35e5b2e10eba8dc7b91ab3469f6f1085f
Evgenii Shumilov <mechnotech@yandex.ru>
2020-07-28 21:14:58 +0300

    add User to models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from pytils.translit import slugify
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class BaseCatalog(models.Model):
