4e058e3c49f2cd1f50491eb51d432c39020301ad
Evgenii Shumilov <mechnotech@yandex.ru>
2020-07-28 17:43:11 +0300

    add models.py and admin.py in API-app

---
+++ api_yamdb/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Review, Comment
+
+
+class RewiewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'author', 'text', 'pub_date', 'score')
+    search_fields = ('text',)
+    list_filter = ('pub_date', 'score',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'review', 'author', 'text', 'pub_date')
+    search_fields = ('text',)
+    list_filter = ('pub_date', 'author',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Review, RewiewAdmin)
+admin.site.register(Comment, CommentAdmin)
