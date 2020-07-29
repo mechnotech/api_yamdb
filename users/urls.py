da8e16f67aa58f1167f9fda4d2593596502fe20b
Muffin Knight <muffin@lukojo.com>
2020-07-29 15:22:34 +0300

    add views for users, add permissions

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from users.views import Token
+from users.views import EmailConfirmationCode, UsersViewSet, Token, UserViewSet
+
+users_router_v1 = DefaultRouter()
+# users_router_v1.register('users/(?P<username>.+)', UserViewSet)
+users_router_v1.register('users', UsersViewSet)
 
 urlpatterns = [
-    # path('auth/token/', YamTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/<slug:username>/', UserViewSet.as_view()),
     path('auth/token/', Token.as_view(), name='token_obtain_pair'),
+    path('auth/email/', EmailConfirmationCode.as_view(), name='email'),
+    path('', include(users_router_v1.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,34 @@
+from random import randint
+
 from django.core.mail import send_mail
-from rest_framework import status
+from django.db.models import ObjectDoesNotExist
+from rest_framework import status, viewsets, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from users.serializers import YamRegUserSerializer
+from users.models import YamUser
+from users.permissions import IsAdmin
+from users.serializers import YamRegUserSerializer, YamUsersSerializer, TokenSerializer
 
 
-class Token(APIView):
+class UsersViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdmin,)
+    queryset = YamUser.objects.all()
+    serializer_class = YamUsersSerializer
+
+
+class UserViewSet(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAdmin,)
+    pagination_class = None
+    queryset = YamUser.objects.all()
+    lookup_field = 'username'
+    serializer_class = YamUsersSerializer
+
+
+
+
+class EmailConfirmationCode(APIView):
     # permission_classes = [permissions.IsAuthenticated]
 
     # def get(self, request, format=None):
@@ -14,20 +36,40 @@ class Token(APIView):
     #     serializer = PollSerializer(polls, many=True)
     #     return Response(serializer.data)
 
-    def post(self, request, format=None):
+    def post(self, request):
         email = request.data['email']
         serializer = YamRegUserSerializer(data={'email': email})
 
         if serializer.is_valid():
-            serializer.save(username=email)
-            code = 123
+            code = randint(100000, 999999)
+            serializer.save(username=email, code=code)
             send_mail(
                 'Confirmation Code',
                 f'Hi, there. This is your code: {code}',
-                'from@example.com',
+                'security@yamdb.fake',
                 [email],
                 fail_silently=False,
             )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class Token(APIView):
+    def post(self, request):
+        email = request.data['email']
+        code = int(request.data['code'])
+
+        try:
+            user = YamUser.objects.get(email=email)
+        except ObjectDoesNotExist:
+            user = None
+
+        serializer = TokenSerializer(user, data=request.data)
+        serializer.is_valid()
+        if user is not None:
+            if user.code == code:
+                refresh = RefreshToken.for_user(user)
+                return Response(data={'token': str(refresh.access_token)}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # return Response(data={'error': 'error', 'email': email, 'code': code}, status=status.HTTP_400_BAD_REQUEST)
