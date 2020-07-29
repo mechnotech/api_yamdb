2242790001f2a2382ee01f690bcfc0a1f1a6a1ba
Evgenii Shumilov <mechnotech@yandex.ru>
2020-07-29 16:03:40 +0300

    Add serializers for review and comments

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category, Genre, Title
+from .models import Category, Genre, Title, Review, Comment
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -19,3 +19,15 @@ class TitleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Title
 		fields = ['categoty', 'genre', 'name', 'year']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Review
+		fields = ['title', 'author', 'text', 'score', 'pub_date']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Comment
+		fields = ['review', 'author', 'text', 'pub_date']
