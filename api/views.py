200311df5e88984b1cc55bc6127b4156303e15d8
Dmitry Morozov <inood@yandex.ru>
2020-07-29 19:30:51 +0700

    add Titles, Category, Genres + pytils for Sluglify

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Category, Genre, Title
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class CategoriesViewSet(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
