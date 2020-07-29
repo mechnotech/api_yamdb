92ad632fd22127b8938232feb8533fea9c7c665c
Evgenii Shumilov <mechnotech@yandex.ru>
2020-07-29 18:05:51 +0300

    Add ViewSets for review and comments

--- api/views.py
+++ api/views.py
@@ -1,10 +1,20 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Category, Genre, Title
-from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+from .models import Category, Genre, Title, Review, Comment
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer, ReviewSerializer, CommentSerializer
 
 
 class CategoriesViewSet(generics.ListAPIView):
     queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ReviewsViewSet(viewsets.GenericViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class CommentsViewSet(viewsets.GenericViewSet):
+    queryset = Comment.objects.all()
     serializer_class = CategorySerializer
