a7f8cfbec296865ebcb65e3790978908a3623f11
Muffin Knight <muffin@lukojo.com>
2020-07-29 03:10:09 +0300

    make tokenization. add code, role in user model.

--- users/models.py
+++ users/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 
 class YamUser(AbstractUser):
+    ROLES = [
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin'),
+    ]
+
     bio = models.TextField(blank=True)
+    code = models.IntegerField()
+    role = models.CharField(
+        max_length=9,
+        choices=ROLES,
+        default='user',
+    )
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from users.views import Token
+from users.views import EmailConfirmationCode, UsersViewSet, Token
+
+users_router_v1 = DefaultRouter()
+users_router_v1.register('users', UsersViewSet)
 
 urlpatterns = [
-    # path('auth/token/', YamTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', include(users_router_v1.urls)),
     path('auth/token/', Token.as_view(), name='token_obtain_pair'),
+    path('auth/email/', EmailConfirmationCode.as_view(), name='email'),
 ]
