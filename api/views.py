b9a5880574f8905652069c8c637dd85692ec2fca
Евгений Шумилов <mechnotech@gmail.com>
2020-07-30 13:50:45 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -10,11 +10,19 @@ class CategoriesViewSet(viewsets.ModelViewSet):
 
 
 class ReviewsViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
+
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        self.queryset = title.reviews.all()
+        return self.queryset
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    
+    serializer_class = CommentSerializer    
