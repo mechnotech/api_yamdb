895e5b6ebbd6219808ed19ae08e3a1601e7844fe
Dmitry Morozov <inood@yandex.ru>
2020-08-03 19:39:36 +0700

    Mixins fix + router rename

--- api/urls.py
+++ api/urls.py
@@ -4,26 +4,26 @@ from rest_framework.routers import DefaultRouter
 from .views import (CategoriesViewSet, CommentsViewSet, GenresViewSet,
                     ReviewsViewSet, TitlesViewSet)
 
-users_router_v1 = DefaultRouter()
+router_v1 = DefaultRouter()
 
-users_router_v1.register('categories', CategoriesViewSet,
-                         basename='categories')
-users_router_v1.register('genres', GenresViewSet,
-                         basename='genres')
-users_router_v1.register('titles', TitlesViewSet,
-                         basename='titles')
+router_v1.register('categories', CategoriesViewSet,
+                   basename='categories')
+router_v1.register('genres', GenresViewSet,
+                   basename='genres')
+router_v1.register('titles', TitlesViewSet,
+                   basename='titles')
 
-users_router_v1.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewsViewSet,
     basename='reviews'
 )
-users_router_v1.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentsViewSet,
     basename='comments'
 )
 
 urlpatterns = [
-    path('v1/', include(users_router_v1.urls))
+    path('v1/', include(router_v1.urls))
 ]
